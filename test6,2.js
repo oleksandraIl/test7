f3067b4ac5212978d71edb4931180b15a568d3ad
oleksandraIl <144255710+oleksandraIl@users.noreply.github.com>
2023-10-30 18:25:33 +0200

    test6,2.js

---
+++ test6,2.js
@@ -0,0 +1,12 @@
+const fibonacci = function fib(n) {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
+  }
+  
+console.log(fibonacci(30))
